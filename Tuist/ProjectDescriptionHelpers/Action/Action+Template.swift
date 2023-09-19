9757d4e1c75ed16540002abc172aedd71fe34449
Hamp <yongbeomkwak@gmail.com>
2023-09-19 15:37:07 +0900

    [CHORE]

--- Tuist/ProjectDescriptionHelpers/Action/Action+Template.swift
+++ Tuist/ProjectDescriptionHelpers/Action/Action+Template.swift
@@ -1,5 +1,6 @@
 import ProjectDescription
 
+/// 쉘 스크립트 관리
 public extension TargetScript {
     static let swiftLint = TargetScript.pre(
         path: Path.relativeToRoot("Scripts/SwiftLintRunScript.sh"),
